fe483e8c685561b5bd91184aac38883f2172d703
John David Gonzalez <jhndagon@gmail.com>
2020-04-25 19:45:19 -0500

    Models created

---
+++ src/room/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    currency: { type: String },
+    description: { type: String },
+    images: [{ type: String }],
+    property_name: { type: String },
+    tumbnails: { type: String },
+    price: { type: Number },
+    rating: { type: Number },
+    agency: { type: Schema.Types.ObjectId, ref: "Agency" },
+    location: { type: Schema.Types.ObjectId, ref: "Location"}
+});
+
+module.exports = mongoose.model("Room", roomSchema);
