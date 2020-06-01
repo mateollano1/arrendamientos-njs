d7cf18567a81b36100d9f3740dd8943287928a44
Steve <steve.blandon@udea.edu.co>
2020-06-01 18:32:04 -0500

    test endpoints booking, agency and location

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -16,7 +16,6 @@ describe("test endpoint rooms",()=>{
         expect(response.status).toBe(200);
         expect(response.type).toBe('application/json');
         expect(response.body).not.toBeNull();
-        //expect(response.body.result).toBe(7)
         done();
     })
     test('get the endpoint rooms/search with invalid range of dates', async done=>{
@@ -59,7 +58,7 @@ describe("test endpoint rooms",()=>{
         expect(response.body.Message).toBe("Room not found ")
         done();
     })
-    test('get the endpoint rooms/create with all fields ok', async done=>{
+    test('post the endpoint rooms/create with all fields ok', async done=>{
         const response = await request.post('/api/rooms/create').send({
             currency: "COP",
             description: "Casa con vista al mar",
@@ -75,10 +74,9 @@ describe("test endpoint rooms",()=>{
         expect(response.status).toBe(200);
         expect(response.type).toBe('application/json');
         expect(response.body).not.toBeNull();
-        //expect(response.body.id).toBe("5ed540c7d109422e4cbb9198")
         done();
     })
-    test('get the endpoint rooms/create with empty location ', async done=>{
+    test('post the endpoint rooms/create with empty location ', async done=>{
         const response = await request.post('/api/rooms/create').send({
             currency: "COP",
             description: "Casa con vista al mar",
@@ -99,3 +97,133 @@ describe("test endpoint rooms",()=>{
     })
 })
 
+describe("test endpoint location",()=>{
+    test('get the endpoint location', async done=>{
+        const response = await request.get('/api/location/location');
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        done();
+    })
+    test('post the endpoint location', async done=>{
+        const response = await request.post('/api/location/location').send({
+            latitude: "4216",
+            longitude:"1254",
+            code: "IBE",
+            name:"Ibagué"
+          });
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        done();
+    })
+})
+
+describe("test endpoint agency ",()=>{
+    test('get the endpoint agency', async done=>{
+        const response = await request.get('/api/agency/agency');
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        done();
+    })
+    test('post the endpoint agency ', async done=>{
+        const response = await request.post('/api/agency/agency').send({
+            name: "agency test",
+            logo: "logo test"
+          });
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        expect(response.body.Message).toBe("Agency created")
+        done();
+    })
+})
+
+describe("test endpoint booking",()=>{
+    test('post the endpoint booking  ok', async done=>{
+        const response = await request.post('/api/booking').send({
+            checkin: "2021-06-06",
+            checkout: "2021-06-10",
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        done();
+    })
+    test('post the endpoint booking  invalid dates, checkin lower than sysdate', async done=>{
+        const response = await request.post('/api/booking').send({
+            checkin: "2021-06-06",
+            checkout: "2021-06-10",
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        done();
+    })
+
+
+    test('post the endpoint booking dates with invalid checkin date', async done => {
+        const response = await request.post('/api/booking').send({
+            checkin: "2020-05-22",
+            checkout: "2020-06-08",
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(400);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        expect(response.body.error).toBe("Inconsistencia en datos ingresados")
+        done();
+      });
+      test('post the endpoint booking dates with invalid checkout date', async done => {
+        const response = await request.post('/api/booking').send({
+            checkin: "2021-05-22",
+            checkout: "2021-05-20",
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(400);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        expect(response.body.error).toBe("Inconsistencia en datos ingresados")
+        done();
+      });
+      test('post the endpoint booking dates with undefined checkin date', async done => {
+        const response = await request.post('/api/booking').send({
+            checkin: undefined,
+            checkout: "2021-05-20",
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(400);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        expect(response.body.error).toBe("Inconsistencia en datos ingresados")
+        done();
+      });
+      test('post the endpoint booking dates with undefined checkout date', async done => {
+        const response = await request.post('/api/booking').send({
+            checkin: "2021-05-31",
+            checkout: undefined,
+            email: "pedro@correo.com",
+            name: "Pedro",
+            id_room: "5ed54112d109422e4cbb9199"
+          });
+        expect(response.status).toBe(400);
+        expect(response.type).toBe('application/json');
+        expect(response.body).not.toBeNull();
+        expect(response.body.error).toBe("Inconsistencia en datos ingresados")
+        done();
+      });
+    
+})
