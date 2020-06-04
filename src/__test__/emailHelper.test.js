b71a2f82f9d2e2582e6958f8c35de3cc510ea90e
Mateo Llano <mateo.llano1@gmail.com>
2020-06-03 21:45:55 -0500

    date format fixxed

--- src/__test__/emailHelper.test.js
+++ src/__test__/emailHelper.test.js
@@ -1,16 +1,16 @@
 const { getDays, getFormatDate } = require('../helpers/email/emailHelper');
 
 describe('Testing email helper', () => {
-  test('Testing getDays', () => {
-    const response = getDays('2020-05-05', '2020-05-15');
-    expect(response).toEqual(10);
-  });
-  test('Testing getDays with checkin greater than checkout', () => {
-    const response = getDays('2020-05-15', '2020-05-05');
-    expect(response).toEqual(10);
-  });
-  test('Testing getFormatDate', () => {
-    const response = getFormatDate('2020-05-15');
-    expect(response).toEqual('14/5/2020');
-  });
+    test('Testing getDays', () => {
+        const response = getDays('2020-05-05', '2020-05-15');
+        expect(response).toEqual(10);
+    });
+    test('Testing getDays with checkin greater than checkout', () => {
+        const response = getDays('2020-05-15', '2020-05-05');
+        expect(response).toEqual(10);
+    });
+    test('Testing getFormatDate', () => {
+        const response = getFormatDate('2020-05-15');
+        expect(response).toEqual('15/5/2020');
+    });
 });
